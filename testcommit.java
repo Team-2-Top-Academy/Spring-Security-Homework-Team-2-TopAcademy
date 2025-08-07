847dcefb26318afea16ffc99942d87bcd936d646
Jackie Weckie <154088788+JackieWeckie@users.noreply.github.com>
2025-08-07 17:15:46 +0300

    testcommit

---
+++ testcommit.java
@@ -0,0 +1,7 @@
+package com.springsecurityhomeworkteam2;
+
+public class testcommit {
+    public static void main(String[] args) {
+        System.out.println("eto prosto test kotory  nado budet potom udalit");
+    }
+}
